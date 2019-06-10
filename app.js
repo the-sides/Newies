6ac2b0346b21d9163a5756379a5a01d31114e038
the-sides <sides798@gmail.com>
2019-06-10 02:28:59 -0400

    favicon support

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
