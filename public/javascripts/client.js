949e9d9b6404ec15eec565718c45e3926074c429
the-sides <sides798@gmail.com>
2019-06-10 02:29:27 -0400

    promises for user connection status

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,19 +3,30 @@ $(() =>{
     // Runs twice, once failing when the page is initially accessed, and 
     var token = window.location.href.indexOf("access_token=");
     var trackLimit = 20
-    console.log("Boom, let's get these songs ", token+13)
+    const data = {
+        token: undefined
+    }
+
     if(token !== -1){
+        // Info found in url
         token = window.location.href
             .substring(
                 token+13,
                 window.location.href.indexOf('&') 
             )
         console.log(token)
+        data.token = token;
+
+        // Start user breakdown
+        revealStatus('connected', data.token);
+
     }
 
     $('#authoBtn').click(() => {
         const scopes = [
             'playlist-read-private',
+            'user-read-recently-played',
+            'user-top-read',
             'playlist-modify-public',
             'playlist-read-collaborative',
         ]
@@ -26,4 +37,41 @@ $(() =>{
         if(token === -1) return;
         console.log(token)
     })
+
+    function renderUserData(userData){
+        console.log('rendering', userData)
+    }
+
+    function getUserData(_token){ 
+        return new Promise(function(resolve, reject){
+            let headerss = new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + _token
+            })
+            let spotifyRequest = new Request('https://api.spotify.com/v1/me/',{
+                method: 'GET',
+                mode: 'cors',
+                headers: headerss
+            })
+            
+            fetch(spotifyRequest)
+            .then(
+                result => {return result.json()}
+            )
+            .then(
+                (json) => resolve(json)
+            )
+            .catch(error => reject(error))
+        })
+    }
+
+    
+    function revealStatus(stage, token){
+        switch(stage){
+            case 'connected': 
+                getUserData(token).then(rv => renderUserData(rv))
+                break;
+        }
+    }
 })
