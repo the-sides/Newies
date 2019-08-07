110cd0f3c8aee21b47bb58887169c92707665d98
the-sides <sides798@gmail.com>
2019-08-07 13:36:48 -0400

    build system with sass in place

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const favicon = require('serve-favicon');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', indexRouter);
-app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
+app.use(favicon(path.join(__dirname, 'dist/images/favicon.ico')))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
