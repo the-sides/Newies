b6201ebc881cb1613b60fc6b448dcf90a160bc5d
the-sides <sides798@gmail.com>
2019-09-10 11:59:18 -0400

    user information in feed

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -2,12 +2,14 @@
 function displayUser(user) {
     console.log('User Connected', user.display_name)
     const photoSpot = document.querySelector('figure.userPhoto > img');
+    const userNameSpot = document.querySelector('p.userName')
     photoSpot.src = user.images[0].url;
+    userNameSpot.textContent = user.display_name;
 }
 
 function displayPlaylistCount(total) {
     const playlistElem = document.querySelector('.playlistCount');
-    playlistElem.textContent = `You have ${total} playlists`;
+    playlistElem.textContent = `Found ${total} playlists`;
 }
 
 function initRing(total) {
@@ -17,5 +19,10 @@ function initRing(total) {
 function revealFeed() {
     const feed = document.querySelector('.feed');
     feed.classList.remove('feed--hidden');
+}
+function postFeed(message){
+    let newElm = document.createElement('div');
+    newElm.className = 'feedItem';
+
 }
 export { displayUser, displayPlaylistCount, initRing, revealFeed }
